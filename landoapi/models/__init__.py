eb1ecfed8f432c76b2c9ea412102e30dc08626ca
Bastien Abadie <bastien@nextcairn.com>
2019-10-01 14:23:36 +0200

    Add uplift model

--- landoapi/models/__init__.py
+++ landoapi/models/__init__.py
@@ -1,4 +1,5 @@
 from landoapi.models.secapproval import SecApprovalRequest
 from landoapi.models.transplant import Transplant
+from landoapi.models.uplift import Uplift
 
-__all__ = ["SecApprovalRequest", "Transplant"]
+__all__ = ["SecApprovalRequest", "Transplant", "Uplift"]
